66b3dbaa1ec0010651489b1ce8fd5ab92dcc0231
Matthias Gilch <matthias.gilch.mg@gmail.com>
2018-03-06 01:42:29 +0100

    Parse proto files in setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,47 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+from subprocess import check_call as call
+import shlex
+import os
 
 
-with open('README.rst', 'r') as f:
-    long_description = f.read()
+class BuildPBFClasses(install):
+    def run(self):
+        call(' '.join(shlex.split(
+            'protoc -I proto/ --python_out=./parosm/parse/ proto/fileformat.proto proto/osmformat.proto'
+        )), shell=True)
+        install.run(self)
 
-setup(
-    name='parosm',
-    version='0.1b1',
-    description='OpenStreetMap Parser',
-    long_description=long_description,
-    url='https://github.com/DaGuich/parosm.git',
-    keywords=[
-        'parser',
-        'osm',
-        'openstreetmap'
-    ],
-    license='GPL Version 3',
-    classifiers=[
-        'Programming Language :: Python :: 3'
-    ],
-    packages=find_packages(exclude=['tests']),
-    entry_points={
-        'console_scripts': [
-            'osm2sql = parosm.prog.osm2sql.__init__:main',
-            'osminfo = parosm.prog.osminfo.__init__:main'
-        ]
-    },
-    test_suite='tests',
-    python_requires='>=3.0.0'
-)
+
+if __name__ == '__main__':
+    with open('README.rst', 'r') as f:
+        long_description = f.read()
+
+    setup(
+        name='parosm',
+        version='0.1b1',
+        description='OpenStreetMap Parser',
+        long_description=long_description,
+        url='https://github.com/DaGuich/parosm.git',
+        keywords=[
+            'parser',
+            'osm',
+            'openstreetmap'
+        ],
+        license='GPL Version 3',
+        classifiers=[
+            'Programming Language :: Python :: 3'
+        ],
+        packages=find_packages(exclude=['tests']),
+        entry_points={
+            'console_scripts': [
+                'osm2sql = parosm.prog.osm2sql.__init__:main',
+                'osminfo = parosm.prog.osminfo.__init__:main'
+            ]
+        },
+        test_suite='tests',
+        python_requires='>=3.0.0',
+        cmdclass={
+            'install': BuildPBFClasses,
+        }
+    )
