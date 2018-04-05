c19cdaf43d1d072427ff37da04e41b1eab3cc68c
Matthias Gilch <matthias.gilch.mg@gmail.com>
2018-04-05 19:15:31 +0200

    Set Version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     setup(
         name='parosm',
-        version='0.1b1',
+        version='0.1',
         description='OpenStreetMap Parser',
         long_description=long_description,
         url='https://github.com/DaGuich/parosm.git',
