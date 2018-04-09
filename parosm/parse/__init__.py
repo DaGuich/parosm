a9fe7bcfcb8f8a92472dc524cfbfc9ed5a344b70
Matthias Gilch <matthias.gilch.mg@gmail.com>
2018-04-09 21:43:14 +0200

    Export Multiparser

--- parosm/parse/__init__.py
+++ parosm/parse/__init__.py
@@ -4,6 +4,7 @@ Exposes parsers for osm files
 
 from parosm.parse.parsexml import XMLParser
 from parosm.parse.parsepbf import PBFParser
+from parosm.parse.parsemulti import MultiParser
 
 
-__all__ = ('XMLParser', 'PBFParser')
+__all__ = ('XMLParser', 'PBFParser', 'MultiParser')
